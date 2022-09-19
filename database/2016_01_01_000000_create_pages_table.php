dc6631ce8061280a98d33651cd1950a8f71931de
Apurv Bhavsar <apurv.bhavsar.09@gmail.com>
2022-09-19 23:36:33 +0530

    page model resolved in migration file

--- database/2016_01_01_000000_create_pages_table.php
+++ database/2016_01_01_000000_create_pages_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use Coder14\Models\Page;
+use Apurv\LaravelSite\Models\Page;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
