9577a58aebcc41b61dd48b8365cd76af14a3c915
Apurv Bhavsar <apurv.bhavsar.09@gmail.com>
2022-09-20 00:32:28 +0530

    extra module check feature added

--- config/voyager-site.php
+++ config/voyager-site.php
@@ -5,5 +5,20 @@ return [
 
     'homepage-blade' => 'home',
 
-    'default-blade' => 'default'
+    'default-blade' => 'default',
+
+    'extra' => [
+        // [
+        //     'model' => '',
+        //     'slug' => '',
+        //     'scope' => [],
+        //     'slug' => '',
+        //     'seo' => [
+        //         'title' => 'column_name',
+        //         'description' => 'column_name',
+        //         'image' => 'column_name',
+        //     ],
+        //     'template' => 'blade_name', // used default if not present or null
+        // ]
+    ]
 ];
--- src/Http/Controllers/CMSController.php
+++ src/Http/Controllers/CMSController.php
@@ -5,6 +5,7 @@ namespace Apurv\LaravelSite\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Apurv\LaravelSite\Models\Page;
+use Illuminate\Support\Facades\Storage;
 
 class CMSController extends Controller
 {
@@ -21,14 +22,35 @@ class CMSController extends Controller
 
         /**
          * TODO : Add new feature to fetch content from different models like Blog, Post.
-         * IDEA : Create JSON file which store the Model Path, Search Column, Scope, Meta title, Meta description
+         * IDEA : Create JSON data in config file which store the Model Path, Search Column, Scope, Meta title, Meta description
          */
 
         $content = Page::where('slug', $slug)->active()->first();
 
-        if(!$content) abort(404);
+        if(!$content) {
+            $extra = config('voyager-site.extra');
+            if(!$extra) abort(404);
 
-        $this->SetSEO($content);
+            foreach ($extra as $key => $model) {
+                $content = $model['model']::where($model['slug'], $slug)->first();
+                if($content) {
+                    $this->SetSEO($content[$model['seo']['title']], $content[$model['seo']['description']]);
+
+                    if(isset($model['seo']['image'])) {
+                        $images = $this->setImages($content[$model['seo']['image']]);
+                    }
+
+                    if(isset($model['template'])) {
+                        return view($model['template'], compact('content'));
+                    }
+                }
+            }
+
+            abort(404);
+        }
+
+
+        $this->SetSEO($content->title, $content->meta_description);
 
         $blade = $content->template ?? config('voyager-site.default-blade');
         $view = "voyager-site::".config('voyager-site.views').".".$blade;
@@ -36,10 +58,28 @@ class CMSController extends Controller
         return view($view, compact('content'));
     }
 
-    private function SetSEO($content) : void
+    /**
+     * Set title & description for SEO
+     *
+     * @param [String] $title
+     * @param [String] $description
+     * @return void
+     */
+    private function SetSEO($title, $description) : void
     {
-        SEOTools::setTitle($content->title);
-        SEOTools::setDescription($content->meta_description);
+        SEOTools::setTitle($title);
+        SEOTools::setDescription($description);
         SEOTools::setCanonical(request()->url);
+    }
+
+    /**
+     * Check image type and set image for SEO
+     *
+     * @param [Array || String] $images
+     * @return void
+     */
+    private function setImages($images)
+    {
+        SEOTools::addImages([$images]);
     }
 }
